0701a511e306614c98b870c6089a0580c9a2c7bb
rafavtk <rafael.vetrenka@gmail.com>
2023-12-13 17:40:23 -0300

    first commit1

---
+++ KanyeThoughts/KanyeThoughts/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+
+namespace KanyeThoughts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Gerar(object sender, EventArgs e)
+        {
+            PuxarApi();
+        }
+
+        private HttpClient _client = new HttpClient();
+        async Task PuxarApi()
+        {
+            try
+            {
+                Pensamentos pensamentos = new Pensamentos();
+
+                string APIUrl = String.Format("https://api.kanye.rest/");
+                HttpResponseMessage response = await _client.GetAsync(APIUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+
+
+                    //deserializando o json e colocando na classe ParametrosClima()
+                    pensamentos = JsonConvert.DeserializeObject<Pensamentos>(json);
+
+
+                    BindingContext = pensamentos;
+                }
+                else
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                    
+            }
+        }
+    }
+}
---
+++ KanyeThoughts/KanyeThoughts/Pensamentos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KanyeThoughts
+{
+    public class Pensamentos
+    {
+        public string quote {  get; set; }
+    }
+}
